2ccf99d8f9d375d5e6974dd589f3320e4f0111eb
TiEkl <tiekl91@gmail.com>
2020-12-18 12:02:40 +0100

    Added function to check for existing wishlists

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const getWishlist = require('./loaders/wishlistLoader');
+
+
+let listNames = [];
+
+console.log("Welcome to the DiscountWishes app");
+
+async function main() {
+    try {
+        listNames = await getWishlist();
+        console.log("Your saved wishlists: ");
+        listNames.forEach((name, index) => {
+            console.log(`${index + 1}. ${name}`)
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main()
---
+++ src/loaders/wishlistLoader.js
@@ -0,0 +1,27 @@
+const fs = require('fs').promises;
+let listPath = './Wishlists';
+
+async function getWishlist() {
+    let listNames = [];
+    if (await fs.stat(listPath) == null) {
+        await fs.mkdir(listPath);
+    }
+    let files = await fs.readdir(listPath, { withFileTypes: true });
+
+    if (files == null) {
+        throw Error("Something went wrong!");
+    }
+
+    if (files != null) {
+        files.forEach(file => {
+            let splitFile = file.name.split(".");
+            if (splitFile[1].toLowerCase() === "json") {
+                listNames.push(splitFile[0]);
+            }
+        });
+        return listNames;
+    }
+};
+
+module.exports = getWishlist;
+
